d9d9c69d8c2faa0c15e52fb9a42739e59836a249
Andrey Morokhin <morokhin.andrei@gmail.com>
2024-10-18 12:52:56 +0500

    Task 1 Solution

---
+++ src/main/java/ru/oop/task1/Car.java
@@ -0,0 +1,24 @@
+package ru.oop.task1;
+
+
+/**
+ * Класс машины
+ */
+public class Car {
+
+    /**
+     * Конструктор, принимающий человека
+     */
+    public Car(Person person) {
+        //TODO
+    }
+
+    /**
+     *  Изменение позиции человека в машине до точки назначения (destination) или ближайшей к ней точки
+     * @return True если машина смогла доехать до точки назначения, иначе False
+     */
+    public boolean move(Position destination) {
+        //TODO
+        return;
+    }
+}
