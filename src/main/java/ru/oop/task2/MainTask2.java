742c2ef42d5dd2e790c2574bafd7888c66b346d9
Soldatov Aleksey <Fotaxis@mail.ru>
2024-10-22 10:59:42 +0500

    Deleted TODO in task

--- src/main/java/ru/oop/task2/MainTask2.java
+++ src/main/java/ru/oop/task2/MainTask2.java
@@ -28,7 +28,6 @@ public class MainTask2 {
      * @see Person
      * @see Position
      */
-    // TODO реализовать метод moveTo(...)
     public void moveTo(Person person, Position destination, Transport transport) {
         if (person.getPosition() != transport.getPosition()){
             person.walk(transport.getPosition());
